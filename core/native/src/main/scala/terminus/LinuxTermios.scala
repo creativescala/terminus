9339e23e432adaf461707019b514066f301e3e17
Noel Welsh <noel@noelwelsh.com>
2025-01-04 07:04:51 +0000

    Add headers

--- core/native/src/main/scala/terminus/LinuxTermios.scala
+++ core/native/src/main/scala/terminus/LinuxTermios.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 Creative Scala
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package terminus
 
 import scalanative.unsafe.*
@@ -6,7 +22,8 @@ import scalanative.posix
 
 /** The Scala Native implementation of termios.h assumes certain types are
   * implemented as CLong. This is correct on macOS, but not on Linux. This
-  * alternative implementation works on Linux. */
+  * alternative implementation works on Linux.
+  */
 object LinuxTermios extends Termios {
   private val STDIN = scala.scalanative.posix.unistd.STDIN_FILENO
 
