932ec72419fd5bdd2f00fd1e62b6f7f0cab4bece
Paul J Thordarson <kapunga@gmail.com>
2025-02-18 11:02:25 -0500

    Document Termios

--- core/native/src/main/scala/terminus/Termios.scala
+++ core/native/src/main/scala/terminus/Termios.scala
@@ -19,11 +19,33 @@ package terminus
 import scala.scalanative.posix
 import scala.scalanative.unsafe.{Ptr, Zone}
 
+/** An abstraction over the c termios library that contains only the functionality that we need,
+  * hiding the details of reading, updating, and writing terminal settings from the caller. The
+  * details are handled by a [[TermiosAccess]] typeclass instance.
+  *
+  * @see [[TermiosAccess]]
+  * @see [[TermiosStruct]]
+  *
+  * @param accessor An instance of [[TermiosAccess]] used to handle the actual reading, updating,
+  *                 and writing of terminal settings.
+  * @tparam T The termios structure type a Termios instance uses. See [[TermiosStruct]] for more
+  *           details
+  */
 class Termios[T](using accessor: TermiosAccess[T]) {
+  /** Allocates a new termios structure defined by [[this.T]], in the given [[Zone]] and copies
+    * the current terminal settings into it, returning a pointer in memory to this structure.
+    */
   def getAttributes()(using Zone): Ptr[T] = accessor.get
 
+  /** Sets the terminal settings to the attributes defined by the termios structure stored at
+    * the pointer specified by the [[attributes]] argument.
+    */
   def setAttributes(attributes: Ptr[T]): Unit = accessor.set(attributes)
 
+  /** Places the terminal in raw mode.
+    *
+    * @see [[https://en.wikipedia.org/wiki/Terminal_mode Terminal Modes]]
+    */
   def setRawMode(): Unit =
     Zone {
       val attrs = accessor.get
