d822347fd6058714c090282c750483395d4d0440
Paul J Thordarson <kapunga@gmail.com>
2025-02-18 10:05:44 -0500

    Document TermiosStruct

--- core/native/src/main/scala/terminus/TermiosStruct.scala
+++ core/native/src/main/scala/terminus/TermiosStruct.scala
@@ -3,10 +3,21 @@ package terminus
 import scala.scalanative.posix
 import scala.scalanative.unsafe.{CInt, CStruct7}
 
+/** Type aliases for the two possible termios structures, one with CInt bitflags and one with CLong bitflags.
+  * CLong types are the default in scala-native, however linux (and probably more operating systems) use CInt
+  * bitflags. Each of these types has a [[terminus.TermiosAccess]] instance to handle access and manipulation
+  * of termios structures in a platform specific manner.
+  *
+  * @see [[https://pubs.opengroup.org/onlinepubs/7908799/xsh/termios.h.html POSIX `termios.h` Documentation]]
+  * @see [[https://github.com/apple/darwin-xnu/blob/main/bsd/sys/termios.h#L263-L265 Darwin termios.h types]]
+  * @see [[https://github.com/scala-native/scala-native/issues/4143 Scala Native termios linux issue]]
+  */
 object TermiosStruct {
-  private type linux_tcflag_t = CInt // Changed
-  private type linux_speed_t = CInt // Changed
+  // Custom flag types using CInt
+  private type linux_tcflag_t = CInt
+  private type linux_speed_t = CInt
 
+  /** Custom `termios` structure with CInt sized bitflags */
   type cint_flags = CStruct7[
     linux_tcflag_t,     /* c_iflag - input flags   */
     linux_tcflag_t,     /* c_oflag - output flags  */
@@ -17,5 +28,6 @@ object TermiosStruct {
     linux_speed_t       /* c_ospeed - output speed  */
   ]
 
+  /** Scala Native default [[posix.termios.termios termios]] structure with CLong sized bitflags */
   type clong_flags = posix.termios.termios
 }
