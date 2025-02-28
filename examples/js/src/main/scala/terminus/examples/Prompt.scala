de1f2ce75a0e0d6766639449adc157500627c241
Noel Welsh <noel@noelwelsh.com>
2025-02-28 12:00:31 +0000

    Update method name

--- examples/js/src/main/scala/terminus/examples/Prompt.scala
+++ examples/js/src/main/scala/terminus/examples/Prompt.scala
@@ -39,7 +39,7 @@ object Prompt {
   // See https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key
   def read(): Program[Future[KeyCode]] = {
     Terminal
-      .readKeyF()
+      .readKey()
       .flatMap(keyCode =>
         keyCode match {
           case "Enter"     => Future.successful(KeyCode.Enter)
