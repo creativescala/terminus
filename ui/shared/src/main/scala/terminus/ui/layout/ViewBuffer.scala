76a8a9760bc9551982b6192b5b7b58275498c511
Noel Welsh <noel@noelwelsh.com>
2026-06-22 17:09:10 +0100

    Refactor Buffer
    - Buffer putString -> putLine, doesn't handle newlines. Do this with
    Text, as that explicitly represents the expected behavior rather than
    taking a guess.
    
    - We can restrict a Buffer to a rectangle (a "View"), which makes logic
    simpler for components: they always start rendering at (0, 0) and so
    have less geometry to do.
    
    - Catch errors with invalid coordinates.

---
+++ ui/shared/src/main/scala/terminus/ui/layout/ViewBuffer.scala
@@ -0,0 +1,24 @@
+package terminus.ui.layout
+
+/** A Buffer that wraps some underlying Buffer, and rewrites coordinate to match
+  * the transformation provided by [[bounds]].
+  */
+final class ViewBuffer(bounds: Rect, buffer: Buffer):
+  def put(x: Int, y: Int, cell: Cell): Unit =
+    if x >= 0 && x < bounds.right && y >= 0 && y < bounds.bottom then
+      buffer.put(x + bounds.x, y + bounds.y, cell)
+    else ()
+
+  def fill(rect: Rect, cell: Cell): Unit =
+    val x0 = rect.x.max(0)
+    val y0 = rect.y.max(0)
+    val x1 = rect.right.min(bounds.width)
+    val y1 = rect.bottom.min(bounds.height)
+    val r = rect(x0, y0, x1 - x0, y1 - y0)
+    buffer.fill(r, cell)
+
+  def putLine(x: Int, y: Int, l: Line, style: CellStyle): Unit =
+    buffer.putLine(x + bounds.x, y + bounds.y, l, style)
+
+  def view(rect: Rect): Buffer =
+    ViewBuffer(rect, this)
